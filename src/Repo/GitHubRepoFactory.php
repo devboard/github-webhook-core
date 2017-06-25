8051cf39c95d090407d67142f0431adf368dc256
Miro Svrtan <miro@mirosvrtan.me>
2017-06-26 00:12:29 +0200

    Fix GitHubRepoFactory, wasnt using GitHubAccount for owner name

--- src/Repo/GitHubRepoFactory.php
+++ src/Repo/GitHubRepoFactory.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Devboard\GitHub\Webhook\Core\Repo;
 
+use Devboard\GitHub\Account\GitHubAccountLogin;
 use Devboard\GitHub\GitHubRepo;
 use Devboard\GitHub\Repo\GitHubRepoFullName;
 use Devboard\GitHub\Repo\GitHubRepoId;
 use Devboard\GitHub\Repo\GitHubRepoName;
-use Devboard\GitHub\User\GitHubUserLogin;
 
 /**
  * @see GitHubRepoFactorySpec
@@ -40,7 +40,7 @@ class GitHubRepoFactory
         $repo = new GitHubRepo(
             new GitHubRepoId($data['id']),
             new GitHubRepoFullName(
-                new GitHubUserLogin($data['owner']['name']),
+                new GitHubAccountLogin($data['owner']['name']),
                 new GitHubRepoName($data['name'])
             ),
             null,
