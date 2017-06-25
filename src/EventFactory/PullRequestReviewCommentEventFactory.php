8c2e1ffbab19c1665d516a1694ab285465d12b38
Miro Svrtan <miro@mirosvrtan.me>
2017-06-25 22:34:32 +0200

    :#46 PullRequestReviewCommentEvent and Factory

---
+++ src/EventFactory/PullRequestReviewCommentEventFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devboard\GitHub\Webhook\Core\EventFactory;
+
+/**
+ * @see PullRequestReviewCommentEventFactorySpec
+ * @see PullRequestReviewCommentEventFactoryTest
+ */
+class PullRequestReviewCommentEventFactory
+{
+}
