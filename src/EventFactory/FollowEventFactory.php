a7798d933b1fa49cbf7553b3e86c4776ec6913e2
Miro Svrtan <miro@mirosvrtan.me>
2017-06-25 22:19:01 +0200

    :#23 FollowEvent and Factory

---
+++ src/EventFactory/FollowEventFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devboard\GitHub\Webhook\Core\EventFactory;
+
+/**
+ * @see FollowEventFactorySpec
+ * @see FollowEventFactoryTest
+ */
+class FollowEventFactory
+{
+}
